58e81c79d307cc71dbd0dbbd5996a2b7fe89756e
Harrison <waalah33@gmail.com>
2019-10-19 12:46:08 -0500

    Header and Posts

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,19 @@
+from flask import render_template
 from app import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello World!"
+    user = {'username': 'Harrison'}
+    posts = [
+        {
+            'author' : {'username' : 'Joey'},
+            'body': 'This is a post.'
+        },
+        {
+            'author': {'username': 'Henry'},
+            'body': 'This is another post.'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
